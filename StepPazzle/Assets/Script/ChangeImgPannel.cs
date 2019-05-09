0c02a04c55a55eba4c7a7422ac01072c4286d334
Yoshdia <yoshiatsu02@gmail.com>
2019-05-10 03:30:37 +0900

    踏んだら絵が変わる、ステージの生成。　パネルの管理マネージャーを作る

---
+++ StepPazzle/Assets/Script/ChangeImgPannel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeImgPannel : MonoBehaviour {
+    [SerializeField]
+    private Material initMaterial = null;
+    [SerializeField]
+    private Material changeMaterial = null;
+
+    private bool changedTex = false;
+    public bool GetChangeTexFlag() { return changedTex; }
+
+    public void Start()
+    {
+        this.GetComponent<Renderer>().material = initMaterial;
+        changedTex = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && changedTex == false)
+        {
+            this.GetComponent<Renderer>().material = changeMaterial;
+            changedTex = true;
+            Debug.Log("Hit!");
+        }
+    }
+}
---
+++ StepPazzle/Assets/Script/MapCreate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreate : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pannel = null;
+
+    private List<GameObject> pannelList = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < 5; x++)
+        {
+            for (int z = 0; z < 5; z++)
+            {
+                Vector3 pannelPos = new Vector3(x * 1, 0, z * 1);
+                Quaternion pannelQua = new Quaternion();
+                GameObject mapPannel = Instantiate(pannel, pannelPos, pannelQua);
+                pannelList.Add(mapPannel);
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
