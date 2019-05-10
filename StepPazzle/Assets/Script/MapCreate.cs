251ada31370b57b625a83d73d3eecfbf38a7c49b
Yoshdia <yoshiatsu02@gmail.com>
2019-05-11 08:26:45 +0900

    小変更

--- StepPazzle/Assets/Script/MapCreate.cs
+++ StepPazzle/Assets/Script/MapCreate.cs
@@ -7,25 +7,20 @@ public class MapCreate : MonoBehaviour {
     [SerializeField]
     private GameObject pannel = null;
 
-    private List<GameObject> pannelList = new List<GameObject>();
-
-    // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         for (int x = 0; x < 5; x++)
         {
             for (int z = 0; z < 5; z++)
             {
-                Vector3 pannelPos = new Vector3(x * 1, 0, z * 1);
+                Vector3 pannelPos = new Vector3(z * 1, 0, x * 1);
                 Quaternion pannelQua = new Quaternion();
                 GameObject mapPannel = Instantiate(pannel, pannelPos, pannelQua);
-                pannelList.Add(mapPannel);
-
+                yield return new WaitForSeconds(0.1f);
             }
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
 
