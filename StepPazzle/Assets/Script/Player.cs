7b52abe653dfb04c7002c66c0e6e5398b0222452
Yoshdia <yoshiatsu02@gmail.com>
2019-05-15 14:29:23 +0900

    Playerスクリプトをリファクタリング

--- StepPazzle/Assets/Script/Player.cs
+++ StepPazzle/Assets/Script/Player.cs
@@ -7,11 +7,11 @@ public class Player : MonoBehaviour
 {
     // 移動速度
     [SerializeField]
-    private float step = 2f;
+    private float speed=5;
     // 入力受付時、移動先の位置を算出して保存 
-    Vector3 target;
-    // 何らかの理由で移動できなかった場合、元の位置に戻すため移動前の位置を保存
-    Vector3 prevPos;
+    private Vector3 targetPos;
+    //移動方向ベクトル
+    private Vector3 moveVector;
 
     //これから移動するpannelのサイズ
     [SerializeField]
@@ -21,8 +21,10 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+        //移動方向ベクトルの初期化
+        moveVector = new Vector3(0, 0, 0);
         //目的座標をリセット
-        target = transform.position;
+        targetPos = transform.position;
         //アタッチされたオブジェクトのサイズを所得
         movePannelSize = pannel.GetComponent<MeshRenderer>().bounds.size.x;
     }
@@ -30,60 +32,59 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // ① 移動中かどうかの判定。移動中でなければ入力を受付
-        if (transform.position == target)
+        //移動中かどうかの判定。移動中でなければ入力を受付
+        if (transform.position == targetPos)
         {
             SetTargetPosition();
+            //移動先に壁がない場合targetPosを更新させる
+            if (TargetPositionHaveWall() == false)
+            {
+                targetPos = transform.position + moveVector;
+            }
         }
         Move();
     }
 
-    // ② 入力に応じて移動後の位置を算出
+    //入力に応じて移動方向を代入
     void SetTargetPosition()
     {
-        //移動先に壁があるか
-        bool targetPositionNoWallFlag = false;
-
-        prevPos = target;
-
-        Vector3 moveVector = new Vector3(0, 0, 0);
-        //斜め移動を防ぐためのフラグ
-        bool oneBottonInputFlag = false;
-
-        if (Input.GetKey(KeyCode.RightArrow) && oneBottonInputFlag == false)
+        moveVector = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             moveVector.x = +movePannelSize;
-            oneBottonInputFlag = true;
+            return;
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && oneBottonInputFlag == false)
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             moveVector.x = -movePannelSize;
-            oneBottonInputFlag = true;
+            return;
         }
-        if (Input.GetKey(KeyCode.UpArrow) && oneBottonInputFlag == false)
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             moveVector.z = +movePannelSize;
-            oneBottonInputFlag = true;
+            return;
         }
-        if (Input.GetKey(KeyCode.DownArrow) && oneBottonInputFlag == false)
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             moveVector.z = -movePannelSize;
-            oneBottonInputFlag = true;
+            return;
         }
-
-        targetPositionNoWallFlag = Physics.Raycast(transform.position, moveVector, 1);
-
-        if (targetPositionNoWallFlag == false)
-        {
-            target = transform.position + moveVector;
-        }
-
     }
 
-    // ③ 目的地へ移動する
+    //移動先に壁があるか(ある場合trueを返す
+    bool TargetPositionHaveWall()
+    {
+        bool targetPositionNoWallFlag = false;
+        //自分の移動先にレイを飛ばし障害物を検知する
+        targetPositionNoWallFlag = Physics.Raycast(transform.position, moveVector, 1);
+        return targetPositionNoWallFlag;
+    }
+
+    //目的地へ移動する
     void Move()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target, step * Time.deltaTime);
+        //target座標へspeedの速さで進む
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
 
 }
