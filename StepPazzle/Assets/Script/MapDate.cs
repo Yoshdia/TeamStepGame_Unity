c3b5c813b514fea4a0805caa0111de3c23abb25f
haraseiya <seiyamidori@gmail.com>
2019-05-21 20:01:40 +0900

    リファクタリング開始

---
+++ StepPazzle/Assets/Script/MapDate.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDate : MonoBehaviour
+{
+    public enum groundName
+    {
+        eDefaultPannel = 0,
+        eChangedPannel = 1,
+        eWall = 2,
+        ePlayerDefaultPosition = 3
+    }
+
+    int[,] mapDate =
+        {
+        {0,2,0,0,0,2 },
+        {0,0,0,2,0,2 },
+        {0,0,0,0,0,2 },
+        {0,2,2,3,0,2 },
+        {0,0,0,0,0,0 }
+    };
+
+    public int GetMapDate(int x, int y)
+    {
+        return mapDate[y, x];
+    }
+
+    private void Start()
+    {
+        GetPlayerPos();
+    }
+
+    //二次元配列からプレイヤー座標を算出
+    public Vector2 GetPlayerPos()
+    {
+        Vector2 playerPosition = new Vector2(-1,-1);
+
+        bool checkEnd = false;
+
+        for (playerPosition.y = 0; playerPosition.y < mapDate.GetLength(0); playerPosition.y++)
+        {
+            for (playerPosition.x= 0; playerPosition.x < mapDate.GetLength(1); playerPosition.x++)
+            {
+                if (mapDate[(int)playerPosition.y , (int)playerPosition.x] == 3)
+                {
+                    checkEnd = true;
+                    break;
+                }
+            }
+            if (checkEnd == true)
+            {
+                break;
+            }
+        }
+
+        return playerPosition;
+    }
+
+    //二次元配列を返す
+    public int[,] GetStageDate()
+    {
+        return mapDate;
+    }
+
+
+}
---
+++ StepPazzle/Assets/Script/PlayerMoveContloller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveContloller : MonoBehaviour {
+
+    // 移動速度
+    [SerializeField]
+    private float speed = 5;
+    // 入力受付時、移動先の位置を算出して保存 
+    private Vector3 targetPos;
+    //移動方向ベクトル
+    private Vector3 moveVector;
+
+    //地面のオブジェクトをここに保存
+    [SerializeField]
+    private GameObject pannel = null;
+    //移動する距離。pannelにタッチされたオブジェクトのサイズをここに保存する
+    private float movePannelSize;
+
+    private int[,] mapDate;
+
+    void Start () {
+        mapDate=GetComponent<MapDate>().GetStageDate();
+	}
+	
+	void Update () {
+		
+	}
+}
