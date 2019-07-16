6cc877ceda416d1ddc95546017fd90d7596d00c0
AÅÅÅÅÅAAAAAAAAAAAAAAAAAA <AAAAAAAAAAAAAAAAAAAAAAAAAAAAA>
2019-07-16 16:35:11 +0900

    操作方法をタッチパネルに実装できたのかもしれない

--- StepPazzle/Assets/Scripts/InputScreenTouch.cs
+++ StepPazzle/Assets/Scripts/InputScreenTouch.cs
@@ -3,59 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+using UnityEngine.UI;
+
 public class InputScreenTouch : MonoBehaviour
 {
     bool firstTouched = false;
 
-    Vector3 firstMousePos ;
+    Vector3 firstMousePos;
 
     public string inputKey()
     {
         string input = "";
 
+        //Touch touch= Input.GetTouch(0);
         //長押しされている場合のみ処理を行う
-        if (Input.GetMouseButton(0))
+        //if (Input.GetMouseButton(0))
+        foreach (Touch touch in Input.touches)
         {
-            //一番最初に触れられた座標を記憶
-            if (!firstTouched)
+            if (Input.touchCount > 0)
             {
-                firstMousePos = Input.mousePosition;
-                firstTouched = true;
+                //一番最初に触れられた座標を記憶
+                if (!firstTouched)
+                {
+                    //firstMousePos = Input.mousePosition;
+                    firstMousePos = touch.position;
+                    firstTouched = true;
+                }
+
+                //継続的に触れられている座標
+                //Vector3 nowMousePos = Input.mousePosition;
+                Vector3 nowMousePos = touch.position;
+
+                //二つの座標の差
+                //Vector3 firstSubNowPos = firstMousePos - nowMousePos;
+                Vector3 firstSubNowPos = firstMousePos - nowMousePos;
+
+                float x = firstSubNowPos.x;
+                float y = firstSubNowPos.y;
+
+                if (x != 0 || y != 0)
+                {
+
+
+                    //差の値で、xとyどちらが大きいか
+                    string biggerPos = (Math.Abs(x) > Math.Abs(y)) ? "x" : "y";
+
+                    if (biggerPos == "x")
+                    {
+                        input = x >= 0 ? "left" : "right";
+                    }
+                    else if (biggerPos == "y")
+                    {
+                        input = y >= 0 ? "down" : "up";
+                    }
+                }
+
+
             }
-
-            //継続的に触れられている座標
-            Vector3 nowMousePos = Input.mousePosition;
-
-            //二つの座標の差
-            Vector3 firstSubNowPos = firstMousePos - nowMousePos;
-
-            float x = firstSubNowPos.x;
-            float y = firstSubNowPos.y;
-
-            if(x!=0||y!=0)
+            else
             {
-
-
-            //差の値で、xとyどちらが大きいか
-            string biggerPos=(Math.Abs(x) > Math.Abs(y)) ? "x" : "y";
-
-            if(biggerPos=="x")
-            {
-                input = x >= 0 ? "left" : "right";
+                firstTouched = false;
             }
-            else if(biggerPos=="y")
-            {
-                input = y >= 0 ? "down" : "up";
-            }
-            }
-
 
         }
-        else
-        {
-            firstTouched = false;
-        }
-
         return input;
     }
 }
